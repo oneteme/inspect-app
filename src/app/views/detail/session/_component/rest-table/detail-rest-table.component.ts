6e95574ed8a7b20677105894a98535c66d812688
fufu <antonin2.declerck@gmail.com>
2024-11-25 14:50:58 +0100

    filter rest remove

--- src/app/views/detail/session/_component/rest-table/detail-rest-table.component.ts
+++ src/app/views/detail/session/_component/rest-table/detail-rest-table.component.ts
@@ -12,7 +12,6 @@ import {RestRequest} from "src/app/model/trace.model";
 export class DetailRestTableComponent implements OnInit {
     displayedColumns: string[] = ['status', 'host', 'path', 'start', 'duree'];
     dataSource: MatTableDataSource<RestRequest> = new MatTableDataSource();
-    filterTable = new Map<string, any>();
 
     @ViewChild('paginator', {static: true}) paginator: MatPaginator;
     @ViewChild('sort', {static: true}) sort: MatSort;
@@ -28,21 +27,6 @@ export class DetailRestTableComponent implements OnInit {
 
     ngOnInit() {
         this.dataSource.sortingDataAccessor = sortingDataAccessor;
-        this.dataSource.filterPredicate = filterPredicate;
-    }
-    
-    applyFilter(event: Event) {
-        const filterValue = (event.target as HTMLInputElement).value;
-        this.filterTable.set('filter', filterValue.trim().toLowerCase());
-        this.dataSource.filter = JSON.stringify(Array.from(this.filterTable.entries()));
-        if (this.dataSource.paginator) {
-            this.dataSource.paginator.firstPage();
-        }
-    }
-
-    toggleFilter(filter: string[]) {
-        this.filterTable.set('status', filter);
-        this.dataSource.filter = JSON.stringify(Array.from(this.filterTable.entries()));
     }
 
     selectedRequest(event: MouseEvent, row: any) {
@@ -55,22 +39,4 @@ const sortingDataAccessor = (row: any, columnName: string) => {
     if (columnName == "start") return row['start'] as string;
     if (columnName == "duree") return (row["end"] - row["start"]);
     return row[columnName as keyof any] as string;
-}
-
-const filterPredicate = (data: RestRequest, filter: string) => {
-    var map: Map<string, any> = new Map(JSON.parse(filter));
-    let isMatch = true;
-    for (let [key, value] of map.entries()) {
-        if (key == 'filter') {
-            isMatch = isMatch && (value == '' ||
-                (data.host?.toLowerCase().includes(value) || data.method?.toLowerCase().includes(value) || data.query?.toLowerCase().includes(value) ||
-                data.path?.toLowerCase().includes(value)));
-        } else if (key == 'status') {
-            const s = data.status.toString();
-            isMatch = isMatch && (!value.length || (value.some((status: any) => {
-                return s.startsWith(status[0]);
-            })));
-        }
-    }
-    return isMatch;
 }
