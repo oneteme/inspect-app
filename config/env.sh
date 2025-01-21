3b9e4db6ed811cbda4a8c491aaeb98fade9fa367
u$f <usf.alami@gmail.com>
2025-01-21 18:11:31 +0100

    edit

--- config/env.sh
+++ config/env.sh
@@ -3,12 +3,12 @@
 OUT=${1:-"environment.remote.json"}
 
 ENTRIES=""
-for ENTRY in INSPECT_SERVER_URL:host JAVA_HOME:java; do
-  KEY=${ENTRY%:*}
-  VALUE=$(printenv "$KEY")
+for ENTRY in INSPECT_SERVER_URL:host; do
+  VALUE=$(printenv "${ENTRY%:*}")
+  KEY=${ENTRY#*:}
   if [ -n "$VALUE" ]; then
-    ENTRIES="$ENTRIES\"$KEY\":\"${VALUE#*:}\","
-    echo "'$KEY'=$VALUE"
+    ENTRIES="$ENTRIES\"$KEY\":\"$VALUE\","
+    echo "$KEY=$VALUE"
   else
     echo "'$KEY' env. variable not defined"
   fi
