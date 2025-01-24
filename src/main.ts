d565eddc5065d4cf2685c47c1c2f43d7a3f2d404
YoussefDahi <youssefdahi686@gmail.com>
2025-01-24 14:59:27 +0100

    Merge remote config before bootstrapping

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,53 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { app, environment } from './environments/environment';
+import { ApplicationNew } from './app/model/conf.model';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// move this code maybe 
+const HOST_PATTERN = /https?:\/\/[\w\-.]+(:\d{2,5})?\/?/;
+const PERIOD_PATTERN = /LAST_(\d+)(_\d+)?/;
+function loadConfig(){
+  return fetch('assets/environement.remote.json')
+            .then(res => res.json())
+            .then((resp: ApplicationNew) =>{
+                matchRegex(resp.host, "host", HOST_PATTERN,);
+                validate(resp.defaultEnv, "defaultEnv");
+                matchRegex(resp.gridViewPeriod, "gridViewPeriod", PERIOD_PATTERN);
+                matchRegex(resp.kpiViewPeriod, "kpiViewPeriod", PERIOD_PATTERN);
+                console.info(`initialized app config: ${JSON.stringify(app)}`)
+              })
+            .catch((err)=> {
+              console.warn(`Error while loading config, Default config will be used: ${JSON.stringify(app)}`);
+              throw err;
+            })
+  
+}
+
+
+function validate(v: string, name:string){
+  if(v){
+    app[name]= v 
+  }else {
+    console.warn(`bad value ${name}=${v}. Default value will be used: ${app[name]}`);
+  }
+}
+
+ function matchRegex(v: string,  name: string, pattern: RegExp) {
+  if(v && pattern.exec(v)){
+     app[name] = v;
+  }else {
+    console.warn(`bad value ${name}=${v}, pattern=${pattern}. Default value will be used: ${app[name]}`);
+  }
+  
+}
+
+
+
+loadConfig().then(()=>platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err)));
+
+  
