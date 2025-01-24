c8e0a4be32d2da2fb0bc54c352a066c99989278a
YoussefDahi <youssefdahi686@gmail.com>
2025-01-24 15:11:27 +0100

    edit

--- src/main.ts
+++ src/main.ts
@@ -12,12 +12,13 @@ if (environment.production) {
 // move this code maybe 
 const HOST_PATTERN = /https?:\/\/[\w\-.]+(:\d{2,5})?\/?/;
 const PERIOD_PATTERN = /LAST_(\d+)(_\d+)?/;
+const ENV_PATTERN = /[\w-]/;
 function loadConfig(){
   return fetch('assets/environement.remote.json')
             .then(res => res.json())
             .then((resp: ApplicationNew) =>{
                 matchRegex(resp.host, "host", HOST_PATTERN,);
-                validate(resp.defaultEnv, "defaultEnv");
+                matchRegex(resp.defaultEnv, "defaultEnv",ENV_PATTERN);
                 matchRegex(resp.gridViewPeriod, "gridViewPeriod", PERIOD_PATTERN);
                 matchRegex(resp.kpiViewPeriod, "kpiViewPeriod", PERIOD_PATTERN);
                 console.info(`initialized app config: ${JSON.stringify(app)}`)
@@ -26,15 +27,6 @@ function loadConfig(){
               console.warn(`Error while loading config, Default config will be used: ${JSON.stringify(app)}`);
             })
   
-}
-
-
-function validate(v: string, name:string){
-  if(v){
-    app[name]= v 
-  }else {
-    console.warn(`bad value ${name}=${v}. Default value will be used: ${app[name]}`);
-  }
 }
 
  function matchRegex(v: string,  name: string, pattern: RegExp) {
