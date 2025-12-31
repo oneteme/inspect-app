e315f792ee97c535a8e6f7ebb0803d4995ab9a09
YoussefDahi <125285822+YoussefDahi@users.noreply.github.com>
2025-12-31 17:16:28 +0100

    feat: add filter functionality to report tables across various components

--- src/app/views/supervision/_component/report-table/report-table.component.ts
+++ src/app/views/supervision/_component/report-table/report-table.component.ts
@@ -3,18 +3,25 @@ import {MatTableDataSource} from "@angular/material/table";
 import {StackTraceRow} from "../../../../model/trace.model";
 import {MatPaginator} from "@angular/material/paginator";
 import {MatSort} from "@angular/material/sort";
+import {RestRequestDto} from "../../../../model/request.model";
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'report-table',
   templateUrl: 'report-table.component.html'
 })
 export class ReportTableComponent {
+
+  private readonly pipe = new DatePipe('fr-FR');
+
   displayedColumns: string[] = ['date', 'level', 'message', 'action'];
   dataSource: MatTableDataSource<{ date: string, level: string, message: string, stacktrace: StackTraceRow[] }> = new MatTableDataSource([]);
-
+  filterTable =new Map<string, any>();
+  @Input() filterValue: string = '';
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild('sort', {static: true}) sort: MatSort;
 
+  @Input() useFilter: boolean;
   @Input() isLoading: boolean;
   @Input() set data(objects: any[]) {
     if (objects?.length) {
@@ -23,6 +30,13 @@ export class ReportTableComponent {
       }));
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
+      if(this.useFilter){
+        this.dataSource.filterPredicate = this.filterPredicate;
+        if(this.filterValue){
+          this.filterTable.set('filter', this.filterValue.trim().toLowerCase());
+          this.dataSource.filter = JSON.stringify(Array.from(this.filterTable.entries()));
+        }
+      }
     } else {
       this.dataSource = new MatTableDataSource([]);
     }
@@ -32,4 +46,28 @@ export class ReportTableComponent {
   onClickStack(row: any) {
     this.onClick.emit(row);
   }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.filterTable.set('filter', filterValue.trim().toLowerCase());
+    this.dataSource.filter = JSON.stringify(Array.from(this.filterTable.entries()));
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  filterPredicate = (data: { date: string, level: string, message: string, stacktrace: StackTraceRow[] }, filter: string) => {
+    var map: Map<string, any> = new Map(JSON.parse(filter));
+    let isMatch = true;
+    for (let [key, value] of map.entries()) {
+      if (key == 'filter') {
+        isMatch = isMatch && (value == '' ||
+            (data.level?.toLowerCase().includes(value) ||
+                data.message?.toLowerCase().includes(value) ||
+                data.date?.includes(value)
+            ));
+      }
+    }
+    return isMatch;
+  };
 }
